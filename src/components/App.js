e0a959cd4b1bdcef29be38bad976cab569c8ba91
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-05-21 20:31:50 -0400

    Lab Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+import About from "./About.js";
+
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
+//import About from "./src/components/About.js";
+//import Home from "./src/components/Home.js"
+//import NavBar  from "./src/components/NavBar.js";
+
 
 function App() {
   return (
